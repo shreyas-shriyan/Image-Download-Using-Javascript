6fa46e18e46303297f520ea69b78d8e8fdf98e55
Shreyas Shriyan <shreyshriyan@gmail.com>
2022-11-16 23:55:05 +0530

    added comments and restructured code

--- imageDownload.js
+++ imageDownload.js
@@ -1,16 +1,23 @@
-dataToURL(url) {
-    return fetch(url).then((response) => {
-            return response.blob();
-        }).then(blob => {
-            return URL.createObjectURL(blob);
-        });
-}
+const downloadImage = async (imageUrl) => {
+  // function to download image from link and convert it to objectUrl
+  const dataToURL = (url) => {
+    return fetch(url)
+      .then((response) => {
+        return response.blob();
+      })
+      .then((blob) => {
+        return URL.createObjectURL(blob);
+      });
+  };
 
-async downloadImage() {
-        const a = document.createElement("a");
-        a.href = await dataToURL("https://via.placeholder.com/150");
-        a.download = "";
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-}
+  // logic to download to local
+  const a = document.createElement("a");
+  a.href = await dataToURL(imageUrl);
+  a.download = "";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+};
+
+//example usage
+downloadImage("https://via.placeholder.com/150");
